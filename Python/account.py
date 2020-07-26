25532612c9fdfdb15732613030cfaf480824dc80
DidierMatos <d.matostorres@gmail.com>
2020-07-26 03:34:03 -0500

    Creando constructor en python en la clase account y car, importando las clases necesarias y ajustando main.py e instanciando el objeto para utilizar de acuerdo al constructor

--- Python/account.py
+++ Python/account.py
@@ -3,4 +3,14 @@ class Account:
     name        =    str 
     document    =    str
     email       =    str
-    password    =    str
+    password    =    str  
+
+    # similar a this
+    # Métodos Mágicos, estos métodos son llamados automáticamente y estrictamente bajo ciertas reglas
+    # __init__ se encargará de personalizar la instanciación de la clase o inicializar atributos del objeto creado
+    # __new__ construye el objeto
+    def __init__(self, name, document): # constructor
+        self.name  =  name
+        self.document  =  document
+
+        
--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,19 @@
 from car import Car
+from account import Account
 
+# Método principal de python
+# La notación de python se rige mediante tabs o espacios
 if __name__ == "__main__":
     print("Hola Mundo")
 
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+    car = Car("AMS235", Account("Andres Herrera", "ANDA876")) # Instancia del objeto
+    # car.license = "AMS234"
+    # car.driver = "Andres Herrera"
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Martha"
+    car2 = Car("QWE567", Account("Martha", "MAJK765"))
+    # car2.license = "QWE567"
+    # car2.driver = "Martha"
     print(vars(car2))
+    print(vars(car2.driver))
