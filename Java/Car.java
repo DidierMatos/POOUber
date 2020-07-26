d3cefbda357352b2e68b153beac6385dbbfa4907
DidierMatos <d.matostorres@gmail.com>
2020-07-26 10:48:48 -0500

    Instanciando objetos de las clases creadas, es decir variedad de carros y se agrego en JAVA que imprima a los pasajeros

--- Java/Car.java
+++ Java/Car.java
@@ -12,6 +12,6 @@ public class Car {
     }
 
     void printDataCar(){
-        System.out.println("License: " + license + " Name Driver: " + driver.name);
+        System.out.println("License: " + license + " Name Driver: " + driver.name + " Passengers: " + passenger);
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -5,19 +5,20 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
 
-        Car car = new Car("AMSQ123", new Account("Andres Herrera", "AND123"));
+        UberX uberX = new UberX("AMSQ123", new Account("Andres Herrera", "AND123")); //Instanciando objeto
+        uberX.passenger = 3;
+        uberX.printDataCar();
         // car.license = "AMQ123 ";
         // car.driver ="Andres Herrera";
-        car.passenger = 4;
         // System.out.println("Car License: " + car.license);
-        car.printDataCar();
 
-        Car car2 = new Car("QWERTY", new Account("Andrea Herrera", "ANDA123"));
+        // Car car2 = new Car("QWERTY", new Account("Andrea Herrera", "ANDA123"));
+        // car2.passenger = 3;
+        // car2.printDataCar();
         // car2.license = "QWERTY ";
         // car2.driver = "Andrea Herrera";
-        car2.passenger = 3;
         // System.out.println("Car License: " + car2.license);
-        car2.printDataCar();
+        
     }
 
 }
